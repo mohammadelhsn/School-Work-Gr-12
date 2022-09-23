5d42499515358a99f08c8e51b60b10ba6b098ca9
Mohammad El-Hassan <51869374+mohammadelhsn@users.noreply.github.com>
2022-09-23 09:09:00 -0400

    yesss

---
+++ src/Mini assignments/065.py
@@ -0,0 +1,1 @@
+marks = {};course1=input("Course #1 ");mark1=input("Mark #1");marks[course1]=mark1;course2=input("Course #2 ");mark2=input("Mark #2 ");marks[course2]=mark2;course3=input("Course #3 ");mark3=input("Mark #3 ");marks[course3]=mark3;course4=input("Course #4 ");mark4=input("Mark #4 ");marks[course4]=mark4;print(marks);
---
+++ src/Mini assignments/067.py
@@ -0,0 +1,7 @@
+dict_1={ 0: 34, 1: 500, 2: 642, 3: 2, 4: 67, 5: 8000, 6: 234};lowest=0
+for i in range(len(dict_1)):
+    num=dict_1[i]
+    if(lowest==0):lowest=num
+    if(num < lowest):lowest=num
+    continue
+print(f'THe lowest numnber is {lowest}')
