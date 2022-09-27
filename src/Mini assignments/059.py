9ce54ab8171f76ba55603ff43f6cf8e18fad8735
Mohammad El-Hassan <51869374+mohammadelhsn@users.noreply.github.com>
2022-09-27 09:07:06 -0400

    e

--- src/Mini assignments/059.py
+++ src/Mini assignments/059.py
@@ -1,4 +1,1 @@
-classes = ["science", "math", "english", "history", "geo", "physed"]
-print(f"The first three items in the list are: {classes[0:3]}")
-print(f"The middle three items in the list are: {classes[3:5]}")
-print(f"The last two items in the list are: {classes[4:6]}")
+classes = ["science", "math", "english", "history", "geo", "physed"];print(f"The first three items in the list are: {classes[0:3]}");print(f"The middle three items in the list are: {classes[3:5]}");print(f"The last two items in the list are: {classes[4:6]}")
