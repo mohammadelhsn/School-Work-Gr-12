c8c28c7cdbb91edbdfb05f6d8a6c887668462c7f
Mohammad El-Hassan <51869374+mohammadelhsn@users.noreply.github.com>
2022-10-13 09:04:44 -0400

    computer science

---
+++ src/Assignments/assignment3c.py
@@ -0,0 +1,94 @@
+import time
+
+
+def countCheck(string):
+    if len(string) < 8:
+        return False
+    else:
+        return True
+
+
+def numCheck(string):
+    nums = 0
+    for char in string:
+        if char.isdigit():
+            nums += 1
+        else:
+            continue
+
+    if nums < 2:
+        return False
+    else:
+        return True
+
+
+def lowerCheck(string):
+    count = 0
+    for char in string:
+        if char.islower():
+            count += 1
+        else:
+            continue
+
+    if count < 1:
+        return False
+    else:
+        return True
+
+
+def upperCheck(string):
+    count = 0
+    for char in string:
+        if char.isupper():
+            count += 1
+        else:
+            continue
+
+    if count < 1:
+        return False
+    else:
+        return True
+
+
+while True:
+    try:
+        password = input("What is your password ")
+
+        countRes = countCheck(password)
+
+        if countRes == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+        if countRes == False:
+            print("❌ | The password entered doesn't meet the minimum character length")
+            continue
+
+        numberCheck = numCheck(password)
+        if numberCheck == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+        if numberCheck == False:
+            print("❌ | The password doesn't have the minimum required numbers")
+            continue
+
+        lowCheck = lowerCheck(password)
+        if lowCheck == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+        if lowCheck == False:
+            print("❌ | The password doesn't meet the minimum lower char requirements")
+            continue
+
+        upCheck = upperCheck(password)
+
+        if upCheck == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+        if upCheck == False:
+            print(
+                "❌ | The password doesn't meet the minimum requirements for uppercase letters"
+            )
+            continue
+
+    except Exception as e:
+        print(e)
---
+++ src/Assignments/assignment3d.py
@@ -0,0 +1,135 @@
+import random
+import time
+import string
+
+letters = list(string.ascii_letters)
+nums = list(range(1, 10))
+
+
+def generatePass():
+    length = random.randint(1, 8)
+    nu = random.randint(1, 2)
+
+    password = ""
+
+    for i in range(nu):
+        choice = random.choice(nums)
+        password = password + f"{choice}"
+
+    for i in range(length - 2):
+        choice = random.choice(letters)
+        password = password + choice
+
+    return password
+
+
+def countCheck(string):
+    if len(string) < 8:
+        return False
+    else:
+        return True
+
+
+def numCheck(string):
+    nums = 0
+    for char in string:
+        if char.isdigit():
+            nums += 1
+        else:
+            continue
+
+    if nums < 2:
+        return False
+    else:
+        return True
+
+
+def lowerCheck(string):
+    count = 0
+    for char in string:
+        if char.islower():
+            count += 1
+        else:
+            continue
+
+    if count < 1:
+        return False
+    else:
+        return True
+
+
+def upperCheck(string):
+    count = 0
+    for char in string:
+        if char.isupper():
+            count += 1
+        else:
+            continue
+
+    if count < 1:
+        return False
+    else:
+        return True
+
+
+attempts = 0
+
+while True:
+    try:
+        password = generatePass()
+
+        print("Generating password!")
+
+        time.sleep(2)
+
+        print(f"The password is {password}")
+
+        countRes = countCheck(password)
+
+        if countRes == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+        if countRes == False:
+            time.sleep(2)
+            print("❌ | The password entered doesn't meet the minimum character length")
+            attempts = attempts + 1
+            continue
+
+        numberCheck = numCheck(password)
+        if numberCheck == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+        if numberCheck == False:
+            time.sleep(2)
+            print("❌ | The password doesn't have the minimum required numbers")
+            attempts = attempts + 1
+            continue
+
+        lowCheck = lowerCheck(password)
+        if lowCheck == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+        if lowCheck == False:
+            time.sleep(2)
+            print("❌ | The password doesn't meet the minimum lower char requirements")
+            attempts = attempts + 1
+            continue
+
+        upCheck = upperCheck(password)
+
+        if upCheck == True:
+            time.sleep(2)
+            print("✅ | Passed!")
+            attempts += 1
+            print(f"Game over! Took the program {attempts} attempt(s) to get it!")
+            break
+        if upCheck == False:
+            time.sleep(2)
+            print(
+                "❌ | The password doesn't meet the minimum requirements for uppercase letters"
+            )
+            attempts = attempts + 1
+            continue
+
+    except Exception as e:
+        print(e)
