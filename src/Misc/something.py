01ee129dc760b0a9f4a91d51ac14166dee27205e
Mohammad El-Hassan <51869374+mohammadelhsn@users.noreply.github.com>
2022-11-14 08:04:35 -0500

    Update something.py
    fdsj

--- src/Misc/something.py
+++ src/Misc/something.py
@@ -5,19 +5,10 @@ class Button:
     colour: str
     size: int
     text: str
-
-    def __init__(self):
-        pass
-
-    def setColour(self, colour: str):
+    def __init__(self, colour, size, text):
         self.colour = colour
-
-    def setSize(self, size: int):
         self.size = size
-
-    def setText(self, text: str):
         self.text = text
-
     def draw(self):
         t = turtle.Turtle()
         t.color(self.colour)
@@ -28,15 +19,9 @@ class Button:
         t.write(self.text, font=("arial", 10, "bold"), align="center")
 
 
-b1 = Button()
-b1.setColour("cyan")
-b1.setSize(50)
-b1.setText("hello there")
+b1 = Button("cyan",50,"hello there")
 b1.draw()
-b2 = Button()
-b2.setColour("orange")
-b2.setSize(10)
-b2.setText("hi")
+b2 = Button("orange",10,"hi")
 b2.draw()
 
 turtle.exitonclick()
